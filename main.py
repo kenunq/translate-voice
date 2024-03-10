1db6922994922fc61cfdd6328b191f8ddf37019b
Ivan Zabaluev <ivaiika@yandex.ru>
2024-03-10 18:04:30 +0300

    create Translate_voice

--- main.py
+++ main.py
@@ -1,29 +1,72 @@
 import torch
 import sounddevice as sd
 import time
+import queue
+import vosk
+import sys
+from googletrans import Translator
 
-LANGUAGE = 'ru'
-MODEL_ID = 'ru_v3'
-SAMPLE_RATE = 48000
-SPEAKER = 'baya'  # aidar, baya, kseniya, xenia, random
-PUT_ACCENT = True
-PUT_YO = True
 
-device = torch.device('cpu')
-text = 'Рандомный текст.'
+class Translate_voice:
+    LANGUAGE = "en"
+    MODEL_ID = "v3_en"
+    SAMPLE_RATE = 48000
+    SPEAKER = "en_116"
+    PUT_ACCENT = True
+    PUT_YO = True
+    translator = Translator()
+    device_torch = torch.device("cpu")
+    model_torch, _ = torch.hub.load(
+        repo_or_dir="snakers4/silero-models",
+        model="silero_tts",
+        language=LANGUAGE,
+        speaker=MODEL_ID,
+    )
+    samplerate = 44100
+    device = None
+    # sd.default.device = 'digital output'
 
-model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
-                          model='silero_tts',
-                          language=LANGUAGE,
-                          speaker=MODEL_ID
-                          )
+    def __init__(self, path_to_model: str):
+        self.model_vosk = vosk.Model(path_to_model)
+        self.q = queue.Queue()
 
-model.to(device)
+    def _callback(self, indata, frames, time, status):
+        if status:
+            print(status, file=sys.stderr)
+        self.q.put(bytes(indata))
 
-audio = model.apply_tts(text=text, speaker=SPEAKER, sample_rate=SAMPLE_RATE,put_accent=PUT_ACCENT, put_yo=PUT_YO)
+    def start(self):
+        with sd.RawInputStream(
+            samplerate=self.samplerate,
+            blocksize=8000,
+            device=self.device,
+            dtype="int16",
+            channels=1,
+            callback=self._callback,
+        ):
+            rec = vosk.KaldiRecognizer(self.model_vosk, self.samplerate)
+            while True:
+                data = self.q.get()
+                if rec.AcceptWaveform(data):
+                    text = rec.Result().split('"')[-2]
+                    if text:
+                        print(text)
+                        self.play_sound(text)
 
-print(text)
+    def play_sound(self, text: str):
+        self.model_torch.to(self.device_torch)
+        text = self.translator.translate(text, dest=self.LANGUAGE).text
+        audio = self.model_torch.apply_tts(
+            text=text,
+            speaker=self.SPEAKER,
+            sample_rate=self.SAMPLE_RATE,
+            put_accent=self.PUT_ACCENT,
+            put_yo=self.PUT_YO,
+        )
+        sd.play(audio, self.SAMPLE_RATE)
+        time.sleep((len(audio)) / self.SAMPLE_RATE)
+        sd.stop()
 
-sd.play(audio, SAMPLE_RATE)
-time.sleep(len(audio)/SAMPLE_RATE)
-sd.stop()
+
+q1 = Translate_voice("models/model-small")
+q1.start()
